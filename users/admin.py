a4197ea83a7b4fea08dbcc25c90a1338178d3018
unknown <malice@korea.ac.kr>
2021-05-29 14:04:12 +0900

    user done

---
+++ users/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "language",
+                    "birthdate",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
+
+
+# class CustomUserAdmin(admin.ModelAdmin):
+#     pass
+
+
+#    list_display = ("username", "gender", "language", "currency", "superhost")
+#    list_filter = (
+#        "language",
+#        "currency",
+#        "superhost",
+#    )
+
+
+# this ==> @admin.register(models.User) decorator
+# admin.site.register(models.User, CustomUserAdmin)
