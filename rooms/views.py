afeeed7cb3bf76e036260494dd74e7dfa1416129
soundee_laptop <malice@korea.ac.kr>
2021-06-27 16:51:42 +0900

    pagination with django

--- rooms/views.py
+++ rooms/views.py
@@ -8,20 +8,10 @@ from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1) or 1)
+    page = request.GET.get("page")
     # page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=3)
+    rooms = paginator.get_page(page)
+    # print(vars(rooms.paginator))
+    return render(request, "rooms/home.html", {"pages": rooms})
