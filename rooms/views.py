cb77192f97f81c010d56d9949a1d895ffa2594a4
soundee_laptop <malice@korea.ac.kr>
2021-07-03 20:20:44 +0900

    function based view room page

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,35 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
+from django.views.generic import ListView
+from django.http import HttpResponse, Http404
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    # page = int(page or 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=3)
-    rooms = paginator.get_page(page)
-    # print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"pages": rooms})
+class HomeView(ListView):
+    """Homeview Description"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    #   page_kwarg = "potato"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+
+        return context
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+        # return redirect(reverse("core:home"))
